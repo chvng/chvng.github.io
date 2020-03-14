fa015cc325f60665a4fb769d1605e1d4ff93d0b5
chvng <chvngu@gmail.com>
2020-03-14 11:23:21 +0100

    Updates

---
+++ precache-manifest.5fa57cb0d21fb8e4ff5c8f648b03a875.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e29588523d0824e26a6a5b1cd87c61f",
+    "url": "/corona/index.html"
+  },
+  {
+    "revision": "b2f141cb217e5e757198",
+    "url": "/corona/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "949ca478a7f0cc408d56",
+    "url": "/corona/static/js/2.412cf650.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/corona/static/js/2.412cf650.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2f141cb217e5e757198",
+    "url": "/corona/static/js/main.27d3e7b6.chunk.js"
+  },
+  {
+    "revision": "5bdd218fbeda78c2de69",
+    "url": "/corona/static/js/runtime-main.6cd3f02f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/corona/static/media/logo.5d5d9eef.svg"
+  }
+]);
